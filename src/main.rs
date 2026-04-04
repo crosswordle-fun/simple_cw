77a46e8422bcc801a042544b596645ed3624aa6e
elton-cs <govada.elton@gmail.com>
2026-04-03 21:17:37 -0500

    feat: tile v1

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,17 @@ pub mod cw_types;
 pub mod helpers;
 pub mod tile;
 
+use crate::tile::Tile;
 use macroquad::prelude::*;
 
 fn window_conf() -> Conf {
     Conf {
         window_title: "game".to_owned(),
+        window_width: 1280,
+        window_height: 720,
         platform: miniquad::conf::Platform {
             linux_backend: miniquad::conf::LinuxBackend::WaylandOnly,
+
             ..Default::default()
         },
         ..Default::default()
@@ -22,8 +26,25 @@ fn window_conf() -> Conf {
 async fn main() {
     set_default_filter_mode(FilterMode::Nearest);
 
+    let num_tiles_in_row = 11;
+    let grid_size = screen_width() / num_tiles_in_row as f32;
+    let start_idx = num_tiles_in_row / 2 - 2;
+    let end_idx = num_tiles_in_row / 2 + 3;
+
+    let mut tiles = Vec::new();
+    for i in start_idx..end_idx {
+        let pos = Vec2::new(i as f32, 4.);
+        tiles.push(Tile::new_input_tile(
+            pos, grid_size, LIGHTGRAY, DARKGRAY, WHITE,
+        ));
+    }
+
     loop {
         clear_background(BLACK);
+
+        for t in &tiles {
+            t.render();
+        }
         draw_fps();
         next_frame().await;
     }
--- src/tile.rs
+++ src/tile.rs
@@ -1,1 +1,66 @@
+use macroquad::{
+    color::Color,
+    math::{Rect, Vec2},
+    shapes::{draw_rectangle, draw_rectangle_lines},
+};
 
+pub struct Tile {
+    pub pos: Vec2,
+    pub face_rect: Rect,
+    pub base_rect: Rect,
+    pub face_c: Color,
+    pub base_c: Color,
+    pub border_c: Color,
+}
+
+impl Tile {
+    pub fn new_input_tile(
+        pos: Vec2,
+        size: f32,
+        face_c: Color,
+        base_c: Color,
+        border_c: Color,
+    ) -> Self {
+        let w = size;
+        let h = size;
+        let h_base = size / 5.;
+
+        let x = pos.x * size;
+        let y = pos.y * size + pos.y * h_base;
+        let y_base = y + size;
+
+        Self {
+            pos,
+            face_rect: Rect { x, y, w, h },
+            base_rect: Rect {
+                x,
+                y: y_base,
+                w,
+                h: h_base,
+            },
+            face_c,
+            base_c,
+            border_c,
+        }
+    }
+
+    pub fn render(&self) {
+        let x = self.face_rect.x;
+        let y = self.face_rect.y;
+        let w = self.face_rect.w;
+        let h = self.face_rect.h;
+        draw_rectangle(x, y, w, h, self.face_c);
+
+        let x = self.base_rect.x;
+        let y = self.base_rect.y;
+        let w = self.base_rect.w;
+        let h = self.base_rect.h;
+        draw_rectangle(x, y, w, h, self.base_c);
+
+        let x = self.face_rect.x;
+        let y = self.face_rect.y;
+        let w = self.face_rect.w;
+        let h = self.face_rect.h + self.base_rect.h;
+        draw_rectangle_lines(x, y, w, h, 2., self.border_c);
+    }
+}
