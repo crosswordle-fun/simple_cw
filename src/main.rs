c8a0d3f679e6090032bb3972aed689073df20117
elton-cs <govada.elton@gmail.com>
2026-04-03 20:08:07 -0500

    chore: clean up code

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,8 @@ pub mod cw_draws;
 pub mod cw_inputs;
 pub mod cw_types;
 pub mod helpers;
+pub mod tile;
 
-use crate::{
-    cw_draws::{build_wordle_input_bar, build_wordle_input_tiles, build_wordle_level, draw_all_shapes},
-    cw_inputs::handle_wordle_input,
-    cw_types::{WordleGame, WordleInput, new_wordle_level},
-    helpers::create_z_layer_vector,
-};
 use macroquad::prelude::*;
 
 fn window_conf() -> Conf {
@@ -27,22 +22,9 @@ fn window_conf() -> Conf {
 async fn main() {
     set_default_filter_mode(FilterMode::Nearest);
 
-    let mut wordle_input = WordleInput::default();
-    let mut wordle_game = WordleGame::default();
-    wordle_game.curr_level = new_wordle_level(0);
-    println!("{:#?}", wordle_game);
-
     loop {
-        handle_wordle_input(&mut wordle_input, &mut wordle_game.curr_level);
-
-        let mut z_layers = create_z_layer_vector();
         clear_background(BLACK);
-        build_wordle_level(&mut z_layers, &wordle_game.curr_level);
-        build_wordle_input_bar(&mut z_layers);
-        build_wordle_input_tiles(&mut z_layers, &wordle_input);
-        draw_all_shapes(&z_layers);
         draw_fps();
-
         next_frame().await;
     }
 }
