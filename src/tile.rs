e6ff13665b9b3832dc0edb28cb2c92361147f8d4
elton-cs <govada.elton@gmail.com>
2026-04-03 23:32:27 -0500

    feat: tile v2

--- src/tile.rs
+++ src/tile.rs
@@ -2,45 +2,47 @@ use macroquad::{
     color::Color,
     math::{Rect, Vec2},
     shapes::{draw_rectangle, draw_rectangle_lines},
+    text::draw_text,
 };
 
+#[derive(Clone, Copy)]
 pub struct Tile {
     pub pos: Vec2,
     pub face_rect: Rect,
-    pub base_rect: Rect,
+    pub base_height: f32,
+    pub border_size: f32,
     pub face_c: Color,
     pub base_c: Color,
     pub border_c: Color,
+    pub letter: Option<char>,
 }
 
 impl Tile {
     pub fn new_input_tile(
         pos: Vec2,
         size: f32,
+        border_size: f32,
         face_c: Color,
         base_c: Color,
         border_c: Color,
+        letter: Option<char>,
     ) -> Self {
-        let w = size;
-        let h = size;
-        let h_base = size / 5.;
+        let w = size - border_size * 2.;
+        let h = w;
+        let base_height = size / 5.;
 
-        let x = pos.x * size;
-        let y = pos.y * size + pos.y * h_base;
-        let y_base = y + size;
+        let x = pos.x * size + border_size;
+        let y = pos.y * (size + base_height);
 
         Self {
             pos,
             face_rect: Rect { x, y, w, h },
-            base_rect: Rect {
-                x,
-                y: y_base,
-                w,
-                h: h_base,
-            },
+            base_height,
+            border_size,
             face_c,
             base_c,
             border_c,
+            letter,
         }
     }
 
@@ -51,16 +53,22 @@ impl Tile {
         let h = self.face_rect.h;
         draw_rectangle(x, y, w, h, self.face_c);
 
-        let x = self.base_rect.x;
-        let y = self.base_rect.y;
-        let w = self.base_rect.w;
-        let h = self.base_rect.h;
+        let y = self.face_rect.y + self.face_rect.h;
+        let h = self.base_height;
         draw_rectangle(x, y, w, h, self.base_c);
 
-        let x = self.face_rect.x;
         let y = self.face_rect.y;
-        let w = self.face_rect.w;
-        let h = self.face_rect.h + self.base_rect.h;
-        draw_rectangle_lines(x, y, w, h, 2., self.border_c);
+        let h = self.face_rect.h + self.base_height;
+        draw_rectangle_lines(x, y, w, h, self.border_size, self.border_c);
+
+        if self.letter.is_some() {
+            draw_text(
+                self.letter.unwrap().to_string().as_str(),
+                x + self.face_rect.w * 0.25,
+                y + self.face_rect.w * 0.75,
+                self.face_rect.w,
+                self.base_c,
+            );
+        }
     }
 }
